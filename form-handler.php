cd0cffe53e5e91e59390b35a98b00f863c72804b
Rakesh Kumar Singh <73414322+rakeysh45@users.noreply.github.com>
2021-06-26 13:32:55 +0530

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,26 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+
+$email_from = 'rakeyshsingh045@gmail.com'
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+               "User Email: $visitor_email.\n".
+               "Subject: $subject.\n".
+               "User Message: $message.\n";
+
+$to =  'rakeyshsingh45@gmail.com';
+
+$headers = 'From: $email_from \r\n';
+
+$headers = "Reply-to: $visitor_email \r\n";
+
+email($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+?>
